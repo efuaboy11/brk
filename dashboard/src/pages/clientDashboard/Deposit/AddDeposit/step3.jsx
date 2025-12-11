0fc24f69940cf479058aad3064d2b85462790dfa
efuaboy11 <iseghohimhene@gmail.com>
2025-12-11 19:19:44 +0100

    changes

--- dashboard/src/pages/clientDashboard/Deposit/AddDeposit/step3.jsx
+++ dashboard/src/pages/clientDashboard/Deposit/AddDeposit/step3.jsx
@@ -255,13 +255,13 @@ export const AddDeposit3 = () =>{
 
 
                       <div className='col-12 py-3 ms-4'>
-                        <p className="light-text">Please upload the correct Tether transaction receipt for proof.</p>
+                        <p className="light-text">Please upload the correct {details.type} transaction receipt for proof.</p>
                         <p className="light-text">Account will be credited once we have confirmed your payment.</p>
                       </div> 
 
 
                       <div className='mx-4 pt-3 mb-5 pb-5'>
-                        <button className="dashboard-btn width-100 pt-2 pb-3" type="submit" >Paid {formatName(details.name)}</button>      
+                        <button  type="submit" >Paid {formatName(details.name)}</button>      
                         <div className="d-flex justify-content-center pt-2"> 
                           <button onClick={Cancel}  className='text-center py-2 px-4 dashboard-red-btn mt-2 '>Cancel Transaction</button>  
                         </div> 
